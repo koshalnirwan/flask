6ea4220757d1db603d9f9b71e666d02f93cc47f5
Koshal Singh <koshal.nirwan24@gmail.com>
2020-12-03 14:19:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,12 @@
-# import flask dependencies
-from flask import Flask, request, make_response, jsonify
-import pandas as pd
-
-# initialize the flask app
+# app.py
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-#df = pd.read_csv("med_100.csv")
-# default route
+# A welcome message to test our server
 @app.route('/')
 def index():
-  return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Welcome to our server !!</h1>"
 
-#function for responses
-#def results():
-#  # build a request object
-#  req = request.get_json(force=True)  
-#  #fetch action from json
-#  action = req.get('queryResult').get('action')  
-#  if action == 'med_results':
-#    res = calculate_sales(req)
-   
-  #action = 'Hi, I just wanted to check'  
-  #return a fulfillment response
-#  return {'fulfillmentText': res}
-  
-#def calculate_sales(req):
-#  element = req.get('queryResult').get('parameters').get('medicine').get('name')  
-#  med_link  = df[df['lower']==element].values 
-#  result = med_link[0,1]
-#  return(result)
-  
-#create a route for webhook
-@app.route('/webhook', methods=['GET', 'POST'])
-def webhook():
-  # return response
-  #return "Koshal Singh"
-  #return make_response(jsonify(results()))
-  return "<h1>Welcome to my server !!</h1>"
-#run the app
 if __name__ == '__main__':
-  app.run(threaded=True, port=5000)
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
