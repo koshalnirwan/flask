e945d5aaea74f7eee94ebdb45c7bc244dc418e51
Koshal Singh <koshal.nirwan24@gmail.com>
2020-12-03 14:14:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,30 +5,30 @@ import pandas as pd
 # initialize the flask app
 app = Flask(__name__)
 
-df = pd.read_csv("med_100.csv")
+#df = pd.read_csv("med_100.csv")
 # default route
 @app.route('/')
 def index():
   return 'Medical World!'
 
 #function for responses
-def results():
-  # build a request object
-  req = request.get_json(force=True)  
-  #fetch action from json
-  action = req.get('queryResult').get('action')  
-  if action == 'med_results':
-    res = calculate_sales(req)
+#def results():
+#  # build a request object
+#  req = request.get_json(force=True)  
+#  #fetch action from json
+#  action = req.get('queryResult').get('action')  
+#  if action == 'med_results':
+#    res = calculate_sales(req)
    
   #action = 'Hi, I just wanted to check'  
   #return a fulfillment response
-  return {'fulfillmentText': res}
+#  return {'fulfillmentText': res}
   
-def calculate_sales(req):
-  element = req.get('queryResult').get('parameters').get('medicine').get('name')  
-  med_link  = df[df['lower']==element].values 
-  result = med_link[0,1]
-  return(result)
+#def calculate_sales(req):
+#  element = req.get('queryResult').get('parameters').get('medicine').get('name')  
+#  med_link  = df[df['lower']==element].values 
+#  result = med_link[0,1]
+#  return(result)
   
 #create a route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
