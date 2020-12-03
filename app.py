2e04f2f8865f0ec3c8239cd3422249d3e5b4e099
Koshal Singh <koshal.nirwan24@gmail.com>
2020-12-03 22:51:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 # default route
 @app.route('/')
 def index():
-    return """<iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/9e85e733-ec1c-461f-aaba-2e4d7ed71fd2"></iframe>"""
-    #return 'Hello World!'
+    return 'Hello World!'
 
 # function for responses
 def results():
@@ -29,10 +28,12 @@ def results():
         #res = fetch_name(req)
         return {'fulfillmentText': 'Hello'}
   
-#def fetch_name(req):
-#  element = req.get('queryResult').get('parameters').get('medicine').get('name')
-#  result  = list(df["Caution"][df['Name']==element].values)
-#  return(result)
+def fetch_name(req):
+  with open(request.files["portfolios"]) as csvfile:
+            portfolios = csv.DictReader(csvfile)
+  element = req.get('queryResult').get('parameters').get('medicine').get('name')
+  result  = list(df["Caution"][df['Name']==element].values)
+  return(result)
     
 # create a route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
