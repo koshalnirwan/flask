5388b61ec46db78caa05a5e03ca74003477dcef6
Koshal Singh <koshal.nirwan24@gmail.com>
2020-12-04 13:46:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,39 @@
 # import flask dependencies
 from flask import Flask, request, make_response, jsonify
 import pandas as pd
-
 # initialize the flask app
 app = Flask(__name__)
 
-#df = pd.read_csv('https://raw.githubusercontent.com/koshalnirwan/flask/main/New.csv',error_bad_lines=False)
+df = pd.read_csv('New.csv',index_col=0)
+df2 = df.to_dict()
 
-# default route
 @app.route('/')
 def index():
-    return 'hello'
+    return 'Hello World!'
 
 # function for responses
 def results():
     # build a request object
     req = request.get_json(force=True)
-
-    # fetch action from json
+   
+    #fetch action from json
     action = req.get('queryResult').get('action')
     
     if action == 'get_results':
-        return {'fulfillmentText':'This is a response from webhook for color.'}
+        res = 'This is a response from webhook for color.'
     elif action == 'put_results':
-        return {'fulfillmentText':'This is a response from webhook for name.'}
+        res = 'This is a response from webhook for name.'
     elif action == 'set_results':
-        res = fetch_name(req)
-        return {'fulfillmentText': res}
-  
+         res = fetch_name(req)
+         
+    return {'fulfillmentText': res}
 def fetch_name(req):
-  element = req.get('queryResult').get('parameters').get('medicine').get('name')
-  #result  = df.iloc[1:1]
-  return 'Ram Ram'
-    
+    element = req.get('queryResult').get('parameters').get('medicine').get('name')
+    for key,value in df2.items():
+            for k,v in value.items():
+                if element==k:
+                    return v
+                
 # create a route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
@@ -41,21 +42,4 @@ def webhook():
 
 # run the app
 if __name__ == '__main__':
-   app.run()
-
-'''# app.py
-from flask import Flask, request, jsonify
-app = Flask(__name__)
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
-@app.route('/webhook', methods=['GET', 'POST'])
-def webhook():
-  return 'hello world'
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)'''
+   app.run(threaded=True, port=5000)
